3baf1c9d5da9805558663b21ad24801df9b0986b
PJT_LAB <lab1.ssafy@multicampus.com>
2020-01-13 09:18:08 +0900

    Webmobile1 Skeleton Project upload

---
+++ ssafy-frontend/src/services/FirebaseService.js
@@ -0,0 +1,73 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const POSTS = 'posts'
+const PORTFOLIOS = 'portfolios'
+
+// Setup Firebase
+// Do change to your own firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBjQXHHQuw1V5noAgY3-ElbXodJeyV9ki4",
+	authDomain: "p0201wm1.firebaseapp.com",
+	databaseURL: "https://p0201wm1.firebaseio.com",
+	projectId: "p0201wm1",
+	storageBucket: "p0201wm1.appspot.com"
+}
+firebase.initializeApp(firebaseConfig)
+
+const firestore = firebase.firestore()
+
+export default {
+	getPosts() {
+		const postsCollection = firestore.collection(POSTS)
+		return postsCollection
+				.orderBy('created_at', 'desc')
+				.get()
+				.then((docSnapshots) => {
+					return docSnapshots.docs.map((doc) => {
+						let data = doc.data()
+						data.created_at = new Date(data.created_at.toDate())
+						return data
+					})
+				})
+	},
+	postPost(title, body) {
+		return firestore.collection(POSTS).add({
+			title,
+			body,
+			created_at: firebase.firestore.FieldValue.serverTimestamp()
+		})
+	},
+	getPortfolios() {
+		const postsCollection = firestore.collection(PORTFOLIOS)
+		return postsCollection
+				.orderBy('created_at', 'desc')
+				.get()
+				.then((docSnapshots) => {					
+					return docSnapshots.docs.map((doc) => {
+						let data = doc.data()
+						data.created_at = new Date(data.created_at.toDate())
+						return data
+					})
+				})
+	},
+	postPortfolio(title, body, img) {
+		return firestore.collection(PORTFOLIOS).add({
+			title,
+			body,
+			img,
+			created_at: firebase.firestore.FieldValue.serverTimestamp()
+		})
+	},
+	loginWithGoogle() {
+		let provider = new firebase.auth.GoogleAuthProvider()
+		return firebase.auth().signInWithPopup(provider).then(function(result) {
+			let accessToken = result.credential.accessToken
+			let user = result.user
+			return result
+		}).catch(function(error) {
+			console.error('[Google Login Error]', error)
+		})
+	}
+}
