c36bde914c3e8730f4a4e248751ba356ac57ddc9
sht3898 <sehyunteg@naver.com>
2020-01-16 12:13:58 +0900

    구조 변경 | 04

---
+++ ssafy-frontend/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-footer color="grey lighten-2">
+        <div>Hyuntaek's Portfolio</div>
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
---
+++ ssafy-frontend/src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <v-navigation-drawer v-model="sidebar" app>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        {{ appTitle }}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list dense nav>
+                <v-list-item v-for="item in menuItems" :key="item.title" :to="item.path" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+
+        <v-toolbar dense>
+            <span class="hidden-sm-and-up">
+                <v-app-bar-nav-icon @click="sidebar = !sidebar">
+                    <v-icon>{{ "menu" }}</v-icon>
+                </v-app-bar-nav-icon>
+            </span>
+            <v-toolbar-title>
+                <router-link to="/" tag="span" style="cursor: pointer">
+                    {{ appTitle }}
+                </router-link>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn text v-for="item in menuItems" :key="item.title" :to="item.path">
+                    <v-icon left dark>{{ item.icon }}</v-icon>
+                    {{ item.title }}
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        data() {
+            return {
+                appTitle: "Hyuntaek's portfolio",
+                sidebar: false,
+                menuItems: [{
+                        title: 'Portfolio',
+                        path: '/portfolio',
+                        icon: 'people'
+                    },
+                    {
+                        title: 'Post',
+                        path: '/post',
+                        icon: 'folder'
+                    },
+                    {
+                        title: 'Login',
+                        path: '/login',
+                        icon: 'lock_open'
+                    }
+                ]
+
+            }
+        }
+    }
+</script>
